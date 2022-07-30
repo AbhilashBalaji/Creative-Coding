282af29e97d9db7dfb229756114f7b6188ea7faa
AbhilashBalaji <abhilash.balaji1998@gmail.com>
2022-07-30 16:52:40 +0200

    snowflakes

---
+++ snowflakes/snowflakes/sketch.js
@@ -0,0 +1,63 @@
+const CRYSTAL_SIZE = 150
+const SIDES = 6
+
+// layout
+const MARGIN = CRYSTAL_SIZE / 2
+const COLUMNS = 10
+const ROWS = 10
+const PADDING = CRYSTAL_SIZE * 0.2
+const GRIDBOX = CRYSTAL_SIZE + PADDING
+const START = (CRYSTAL_SIZE / 2) + MARGIN
+
+let PALETTE = []
+ALL_CRYSTALS = []
+
+function setup() {
+  const totalX = START + GRIDBOX * COLUMNS
+  const totalY = START + GRIDBOX * ROWS
+  createCanvas(totalX, totalY, SVG)
+
+  PALETTE = [
+    // color(255, 52, 154), // pink
+    // color(4, 0, 152) // blue
+    "#2e313c",
+    "#45404c",
+    "#626375",
+    "#d5cec5",
+    "#f5f5f8",
+  ]
+
+  noLoop()
+  angleMode(DEGREES)
+  rectMode(CENTER)
+}
+
+function draw() {
+  // go to a point on the screen and draw a crystal
+  // continue to do this until we run out of room
+  for (let x = 0; x < COLUMNS; x++) {
+    for (let y = 0; y < ROWS; y++) {
+      const posX = START + (x * GRIDBOX)
+      const posY = START + (y * GRIDBOX)
+      const crystal = makeCrystal({x: posX, y: posY})
+      console.log(crystal)
+      ALL_CRYSTALS.push(crystal)
+    }
+  }
+
+  ALL_CRYSTALS.forEach(crystal => {
+    drawCrystal(crystal)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
